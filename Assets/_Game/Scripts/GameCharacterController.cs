15fb78aa734afdbcc4b3f683ab56103573d10791
umair jamshaid <umairjamshaid.crosshair@gmail.com>
2023-05-23 16:13:07 +0500

    Animator controller implementation with game character controller
    hit, minor attack major attack and die animations working

--- Assets/_Game/Scripts/GameCharacterController.cs
+++ Assets/_Game/Scripts/GameCharacterController.cs
@@ -4,15 +4,166 @@ using UnityEngine;
 
 public class GameCharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum AttackCostType
+    {
+        Stamina,
+        Mana
+    }
+    enum AnimationStates
     {
         
+        IsDizzy,
+        IsVictory,
+        MinorAttackTrigger,
+        MajorAttackTrigger,
+        MajorAttackCompletedTrigger,
+        IsHitTrigger,
+        IsDizzyTrigger,
+        IsDieTrigger,
+        IsVictoryTrigger
+    }
+    [SerializeField] private Animator PlayerAnimator;
+    private int Health = 100;
+    private int Stamina = 100;
+    private int Mana = 100;
+    
+
+
+    public int GetHealth()
+    {
+        return Health;
+    }
+    public void SetHealth(int value)
+    {
+        Health = value;
+    }
+    public int GetStamina()
+    {
+        return Stamina;
+    }
+    public void SetStamina(int value)
+    {
+        Stamina = value;
+    }public int GetMana()
+    {
+        return Mana;
+    }
+    public void SetMana(int value)
+    {
+        Mana = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetDamage(int amount)
     {
-        
+        if(Health > 0)
+        {
+            Health -= amount;
+            PlayerAnimator.SetTrigger(StateToAnimParameter(AnimationStates.IsHitTrigger));
+            if(Health <= 0)
+            {
+                Die();
+            }
+            
+        }
+    }
+    private string StateToAnimParameter(AnimationStates animationState)
+    {
+        string animationStateValue = "";
+        switch (animationState)
+        {
+            // bools
+            
+            case AnimationStates.IsDizzy:
+                {
+                    animationStateValue = "is dizzy";
+                    break;
+                }
+            case AnimationStates.IsVictory:
+                {
+                    animationStateValue = "is victory";
+                    break;
+                }
+            // triggers
+            case AnimationStates.MinorAttackTrigger:
+                {
+                    animationStateValue = "is minor attack trigger";
+                    break;
+                }
+            case AnimationStates.MajorAttackTrigger:
+                {
+                    animationStateValue = "is major attack trigger";
+                    break;
+                }
+            case AnimationStates.MajorAttackCompletedTrigger:
+                {
+                    animationStateValue = "is major attack completed trigger";
+                    break;
+                }
+            case AnimationStates.IsHitTrigger:
+                {
+                    animationStateValue = "is hit trigger";
+                    break;
+                }
+            case AnimationStates.IsDizzyTrigger:
+                {
+                    animationStateValue = "is dizzy trigger";
+                    break;
+                }
+            case AnimationStates.IsDieTrigger:
+                {
+                    animationStateValue = "is die trigger";
+                    break;
+                }
+            case AnimationStates.IsVictoryTrigger:
+                {
+                    animationStateValue = "is victory trigger";
+                    break;
+                }
+            default:
+                animationStateValue = "is idle";
+                break;
+        }
+        return animationStateValue;
+    }
+    public void DoDamage(int amount)
+    {
+        // Damage Enemy
+    }
+    public void DoMinorAttack(int amount)
+    {
+        DoDamage(amount);
+        PlayerAnimator.SetTrigger(StateToAnimParameter(AnimationStates.MinorAttackTrigger));
+
+    }
+    public void DoMajorAttack(int amount, int attackDuration)
+    {
+        DoDamage(amount);
+        PlayerAnimator.SetTrigger(StateToAnimParameter(AnimationStates.MajorAttackTrigger));
+        Invoke("EndMajorAttack", attackDuration);
+    }
+    private void EndMajorAttack()
+    {
+        PlayerAnimator.SetTrigger(StateToAnimParameter(AnimationStates.MajorAttackCompletedTrigger));
+    }
+    public void Die()
+    {
+        PlayerAnimator.SetTrigger(StateToAnimParameter(AnimationStates.IsDieTrigger));
+    }
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            GetDamage(10);
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            DoMajorAttack(10, 5);
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            DoMinorAttack(10);
+        }
+#endif
     }
 }
